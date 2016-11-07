bc1a8702dc77e1ce7d844a98363fb4df258c042e
abarla <haritha.manchili@gmail.com>
2016-11-07 15:03:32 -0600

    Commit

--- src/main/java/org/launchcode/controllers/HelloController.java
+++ src/main/java/org/launchcode/controllers/HelloController.java
@@ -1,28 +1,34 @@
 package org.launchcode.controllers;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.launchcode.models.HelloMessage;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+	
 	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	@ResponseBody
-	public String hello(HttpServletRequest request) {
+	public String helloForm(){
+		return "helloForm";
+	}
+	
+	@RequestMapping(value = "/hello", method = RequestMethod.POST)
+	public String hello(HttpServletRequest request, Model model){
 		
-		// get name parameter from request; will be null of no parameter passed in
 		String name = request.getParameter("name");
 		
-		if (name == null) {
+		//if (name == null || name.equals("")){
+		if(name.isEmpty()){
 			name = "world";
 		}
 		
-		return "<h1>" + HelloMessage.getMessage(name) + "</h1>";
+		//llamda ala java 7/8
+		//name = name != null ? name : "World";
+		
+		model.addAttribute("name", name);
+		return "hello";
 	}
-	
 }
